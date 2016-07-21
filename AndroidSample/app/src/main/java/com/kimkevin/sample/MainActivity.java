d3d0d642181a21484f719712fc331026c7ffc2e8
KimKevin <imkimkevin@gmail.com>
2016-07-22 02:04:51 +0900

    Recover AndroidSample

--- AndroidSample/app/src/main/java/com/kimkevin/sample/MainActivity.java
+++ AndroidSample/app/src/main/java/com/kimkevin/sample/MainActivity.java
@@ -1,38 +1,13 @@
 package com.kimkevin.sample;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-
-import com.kimkevin.sample.adapter.SlidingIconTabAdapter;
-import com.kimkevin.sample.widgets.SlidingTabLayout;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    getSupportActionBar().setElevation(0);
-
-    setContentView(R.layout.activity_slidingtablaout_main);
-
-    SlidingIconTabAdapter adapter = new SlidingIconTabAdapter(getSupportFragmentManager());
-
-    ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-    viewPager.setAdapter(adapter);
-
-    SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.tabs);
-    slidingTabLayout.setCustomTabView(R.layout.tab_img_layout, R.id.tab_name_img);
-    slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-      @Override
-      public int getIndicatorColor(int position) {
-        return Color.WHITE;
-      }
-    });
-
-    slidingTabLayout.setViewPager(viewPager);
+    setContentView(R.layout.activity_main);
   }
 }
-
