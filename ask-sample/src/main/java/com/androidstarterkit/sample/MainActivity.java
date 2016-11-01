7b6333a098da7f60c29038e5cf2c354b6dff1ea7
KimKevin <imkimkevin@gmail.com>
2016-11-01 22:57:51 +0900

    Recover Sample Project

--- ask-sample/src/main/java/com/androidstarterkit/sample/MainActivity.java
+++ ask-sample/src/main/java/com/androidstarterkit/sample/MainActivity.java
@@ -1,23 +1,13 @@
 package com.androidstarterkit.sample;
-import com.androidstarterkit.sample.views.SlidingTabFragment;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    getSupportActionBar().setElevation(0);
-    setContentView(R.layout.activity_sample_main);
-
-    if (savedInstanceState == null) {
-      getSupportFragmentManager()
-          .beginTransaction()
-          .add(R.id.container, new SlidingTabFragment())
-          .commit();
-    }
+    setContentView(R.layout.activity_main);
   }
 }
