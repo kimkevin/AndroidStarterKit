4e590688962102877c33530f5ef534835a61caa2
YongJun Kim <kevin@KimKevin-MacBook-Pro.local>
2017-04-03 23:08:35 +0900

    feat: added website #1

--- ask-remote-module/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ ask-remote-module/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -19,7 +19,7 @@ public final class R {
         public static final int item_touch_helper_swipe_escape_velocity = 0x7f080085;
     }
     public static final class id {
-        public static final int item_touch_helper_previous_elevation = 0x7f0d0006;
+        public static final int item_touch_helper_previous_elevation = 0x7f0d0005;
     }
     public static final class styleable {
         public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f0100f2, 0x7f0100f3, 0x7f0100f4, 0x7f0100f5 };
